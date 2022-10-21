46f8574fd1a2b4dbc5add54a9c175f1728514fcd
jivnshet <jivnshet@gmail.com>
2022-10-21 11:36:48 +0530

    first commit

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/jivnshet/celo-nft-marketplace";
+export const MARKETPLACE_ADDRESS = "0x52Ae22286086263a8Ce57e85cEC4432b32DE9cE9";
